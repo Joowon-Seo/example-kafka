8e352591a827b57445e8c1fd52a6ab2f17e14ec4
Joowon-Seo <90291431+Joowon-Seo@users.noreply.github.com>
2024-06-04 16:55:27 +0900

    git init

---
+++ order-service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -0,0 +1,12 @@
+package com.example.orderservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/external/orders/v1")
+public class OrderController {
+}
